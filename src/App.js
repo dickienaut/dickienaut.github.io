56802660819d781c3ee72350ccbd0fcf8148a2ab
Lyle Dickie <lyle.e.dickie@gmail.com>
2020-04-14 12:25:01 -0400

    add image

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import image from './images/test.jpg'
 import './App.css';
+
+const images = [
+  'https://ibb.co/F0fkQBD',
+  'https://ibb.co/59Sgrp6',
+  'https://ibb.co/Czjqrk3',
+  'https://ibb.co/qj7LMkz'
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <img src={image} alt='profile'/>
+      <p>My name is Lyle and I build things on the web</p>
     </div>
   );
 }
